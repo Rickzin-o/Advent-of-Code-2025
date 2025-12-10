919c7ca9810168b5d49ea6b8700b65fe7cf07c10
José Ricardo <ricardoanimation2@gmail.com>
2025-12-09 23:40:15 -0300

    dia 9 finalizado

--- dia9.py
+++ dia9.py
@@ -28,84 +28,26 @@ print(f"Parte 1 (Executado em {elapsed_time_milliseconds:.3f}ms):", area)
 
 # === DESAFIO 2 ===
 start_time = time.perf_counter()
+from shapely.geometry import Polygon
 
-# Cria um perímetro baseado em pontos sequenciais
-def create_polygon(positions: list):
-    perimeter = set()
-    total_tiles = len(positions)
-    for i, tile in enumerate(positions):
-        next_tile = positions[(i+1) % total_tiles]
-        if tile[0] == next_tile[0]:
-            for i in range(min(tile[1],next_tile[1]), max(tile[1],next_tile[1])+1):
-                perimeter.add((tile[0], i))
-        if tile[1] == next_tile[1]:
-            for i in range(min(tile[0],next_tile[0]), max(tile[0],next_tile[0])+1):
-                perimeter.add((i, tile[1]))
-    return list(perimeter)
-
-points = create_polygon(tile_positions)
-print(points)
-
-
-# Confere se um ponto está entre grupos de pontos
-# ERRADO: Acaba conferindo pontos aleatórios
-def is_between_x_axis(point, tiles, y: int):
-    match_x = list(filter(lambda t: t[1]==y, tiles))
-    comb = list(combinations(match_x, 2))
-    for c in comb:
-        c1, c2 = c
-        print(point, c)
-        if not (c1[1] <= point[1] <= c2[1]) and not(c2[1] <= point[1] <= c1[1]):
-            return False
-    return True
-
-
-def is_between_y_axis(point, tiles, x: int):
-    match_y = list(filter(lambda t: t[1]==x, tiles))
-    comb = list(combinations(match_y, 2))
-    for c in comb:
-        c1, c2 = c
-        print(point, c)
-        if not (c1[0] <= point[0] <= c2[0]) and not(c2[0] <= point[0] <= c1[0]):
-            return False
-    return True
+polygon = Polygon(tile_positions) 
 
 # Loop para pegar o primeiro retângulo com todos os pontos dentro do poligono maior
+
 for p in possible_corners:
     p1, p2 = p
     count = 0
     op_corner1 = (p1[0], p2[1])
     op_corner2 = (p2[0], p1[1])
-    rect = create_polygon([p1, op_corner1, p2, op_corner2])
+    rect = Polygon([p1, op_corner1, p2, op_corner2])
 
-    rect_inside = True
-    print("rect", rect)
-    for i in list(filter(lambda t: t[1]==p1[1], rect)):
-        if not is_between_x_axis(i, points, p1[1]):
-            rect_inside = False
-            break
-    for i in list(filter(lambda t: t[1]==p2[1], rect)):
-        if not is_between_x_axis(i, points, p2[1]):
-            rect_inside = False
-            break
-    for i in list(filter(lambda t: t[0]==p1[0], rect)):
-        if not is_between_y_axis(i, points, p1[0]):
-            rect_inside = False
-            break
-    for i in list(filter(lambda t: t[0]==p2[0], rect)):
-        if not is_between_y_axis(i, points, p2[0]):
-            rect_inside = False
-            break
-
-    if rect_inside:
+    if polygon.contains(rect):
         break
 
-##    break
-            
-    
+area = (abs(p[0][0] - p[1][0]) + 1) * (abs(p[0][1] - p[1][1]) + 1)
 
 end_time = time.perf_counter()
 elapsed_time_seconds = end_time - start_time
 elapsed_time_milliseconds = elapsed_time_seconds * 1000
 
-print(f"Parte 2 (Executado em {elapsed_time_milliseconds:.3f}ms): ")
+print(f"Parte 2 (Executado em {elapsed_time_milliseconds:.3f}ms): ", area)
